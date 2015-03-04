1701dda7965fa271c7d974f063375929270cdc48
Tiago Oliveira <tiago.oliveira@zenvia.com>
2015-03-04 16:16:13 -0300

    Reactive module

--- batch-task-react/src/main/groovy/com/zenvia/react/ReactWorker.groovy
+++ batch-task-react/src/main/groovy/com/zenvia/react/ReactWorker.groovy
@@ -1,9 +1,30 @@
 package com.zenvia.react
 
+import io.vertx.rxcore.java.http.RxHttpClient
+import io.vertx.rxcore.java.http.RxHttpServer
+import org.vertx.groovy.core.http.RouteMatcher
 import org.vertx.groovy.platform.Verticle
 
 class ReactWorker extends Verticle {
 
     def start() {
+
+        def rm = new RouteMatcher()
+        rm.get('/details/:user/:id') { req ->
+            req.response.end "User: ${req.params['user']} ID: ${req.params['id']}"
+        }
+        rm.getWithRegEx('.*') { req ->
+            req.response.sendFile "route_match/index.html"
+        }
+
+        RxHttpServer server = new RxHttpServer(vertx.createHttpServer().requestHandler(rm.asClosure()).toJavaServer())
+                
+        server.http().subscribe({ req ->
+            println("http-server:new-request:${req.path()}")
+            req.response().end("pong:" + req.path())
+        }, { e -> 
+            println("http-server:req-error:" + e)
+        })
+        server.coreHttpServer().listen(8080, "localhost");
     }
 }
